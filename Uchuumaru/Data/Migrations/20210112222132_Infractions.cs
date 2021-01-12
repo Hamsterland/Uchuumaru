91cb9e648fb6e1bdb019dd28354a92ebd2b68296
= <matthewtrip2@gmail.com>
2021-01-12 22:23:37 +0000

    Filter and Infraction migrations

---
+++ Uchuumaru/Data/Migrations/20210112222132_Infractions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Uchuumaru.Migrations
+{
+    public partial class Infractions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Infraction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    SubjectId = table.Column<long>(type: "bigint", nullable: false),
+                    ModeratorId = table.Column<long>(type: "bigint", nullable: false),
+                    GuildId = table.Column<int>(type: "integer", nullable: false),
+                    TimeInvoked = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Duration = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Reason = table.Column<string>(type: "text", nullable: true),
+                    Completed = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Infraction", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Infraction_Guild_GuildId",
+                        column: x => x.GuildId,
+                        principalTable: "Guild",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Infraction_GuildId",
+                table: "Infraction",
+                column: "GuildId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Infraction_ModeratorId",
+                table: "Infraction",
+                column: "ModeratorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Infraction_SubjectId",
+                table: "Infraction",
+                column: "SubjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Infraction");
+        }
+    }
+}
