db774cd8515b0ff64cead41b959c103abe599ad8
= <matthewtrip2@gmail.com>
2021-01-20 21:44:39 +0000

    Create Infraction DTO

--- Uchuumaru/Data/Models/Infraction.cs
+++ Uchuumaru/Data/Models/Infraction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -64,5 +65,31 @@ namespace Uchuumaru.Data.Models
         Kick,
         Warning,
         Filter
+    }
+
+    public class InfractionSummary
+    {
+        public int Id { get; set; }
+        public InfractionType Type { get; set; }
+        public ulong SubjectId { get; set; }
+        public ulong ModeratorId { get; set; }
+        public Guild Guild { get; set; }
+        public DateTime TimeInvoked { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Reason { get; set; }
+        public bool Completed { get; set; }
+
+        public static readonly Expression<Func<Infraction, InfractionSummary>> FromEntityProjection = infraction => new InfractionSummary
+            {
+                Id = infraction.Id,
+                Type = infraction.Type,
+                SubjectId = infraction.SubjectId,
+                ModeratorId = infraction.ModeratorId,
+                Guild = infraction.Guild,
+                TimeInvoked = infraction.TimeInvoked,
+                Duration = infraction.Duration,
+                Reason = infraction.Reason,
+                Completed = infraction.Completed
+            };
     }
 }
