348704dc1d77ee3590d90429dcbbc01a0cad3b47
= <matthewtrip2@gmail.com>
2021-02-02 16:14:32 +0000

    Allow changing other user's birthdays

--- Uchuumaru/Modules/BirthdayModule.cs
+++ Uchuumaru/Modules/BirthdayModule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using Uchuumaru.Preconditions;
 using Uchuumaru.Services.Birthdays;
@@ -20,12 +21,21 @@ namespace Uchuumaru.Modules
         }
 
         [Command("set")]
-        [Summary("Sets a birthday.")]
+        [Summary("Sets your birthday.")]
         public async Task Set(DateTime date)
         {
             var utc = date.ToUniversalTime();
             await _birthday.SetBirthday(Context.User.Id, utc);
-            await ReplyAsync($"{Context.User.Mention} I set your birthday to {date.Month}/{date.Day}.");
+            await ReplyAsync($"{Context.User.Mention}, I set your birthday to {date.Month}/{date.Day}.");
+        }
+        
+        [Command("set")]
+        [Summary("Sets a birthday for a user.")]
+        public async Task Set(IGuildUser user, DateTime date)
+        {
+            var utc = date.ToUniversalTime();
+            await _birthday.SetBirthday(user.Id, utc);
+            await ReplyAsync($"{Context.User.Mention}, I set {user}'s birthday to {date.Month}/{date.Day}.");
         }
 
         [Command("remove")]
@@ -35,6 +45,15 @@ namespace Uchuumaru.Modules
             // default is 01/01/0001 00:00:00
             await _birthday.SetBirthday(Context.User.Id, default);
             await ReplyAsync($"{Context.User.Mention} I removed your birthday.");
+        }
+        
+        [Command("remove")]
+        [Summary("Removes a birthday for a user.")]
+        public async Task Remove(IGuildUser user)
+        {
+            // default is 01/01/0001 00:00:00
+            await _birthday.SetBirthday(user.Id, default);
+            await ReplyAsync($"{Context.User.Mention}, I removed {user}'s birthday.");
         }
     }
 }
