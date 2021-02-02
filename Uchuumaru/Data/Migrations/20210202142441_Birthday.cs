f2ba375e17c4afa7406c65c423de17e5093c592d
= <matthewtrip2@gmail.com>
2021-02-02 15:01:18 +0000

    Birthday Migrations

---
+++ Uchuumaru/Data/Migrations/20210202142441_Birthday.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Uchuumaru.Migrations
+{
+    public partial class Birthday : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthday",
+                table: "Users",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<long>(
+                name: "BirthdayChannelId",
+                table: "Guilds",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "BirthdayChannelId",
+                table: "Guilds");
+        }
+    }
+}
