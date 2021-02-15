9ee98c17a68eab8de53f8bd24dc166e91859aced
= <matthewtrip2@gmail.com>
2021-02-15 02:14:42 +0000

    Create utilities

---
+++ Uchuumaru/Utilities/ChannelExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Discord;
+
+namespace Uchuumaru.Utilities
+{
+    public static class ChannelExtensions
+    {
+        public static string Mention(this IChannel channel) => $"<#{channel.Id}>";
+        public static string Represent(this IChannel channel) => $"{channel.Mention()} ({channel.Id})";
+    }
+}
---
+++ Uchuumaru/Utilities/UserExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Discord;
+
+namespace Uchuumaru.Utilities
+{
+    public static class UserExtensions
+    {
+        public static string Represent(this IUser user) => $"{user.Mention} ({user.Id})";
+    }
+}
